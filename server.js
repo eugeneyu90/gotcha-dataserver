d5d78d30bdc13823d1ec1ee402426a2ba2138dd5
eyze90 <yu.eugene90@gmail.com>
2018-03-20 16:50:41 -0700

    initial setup done

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+// Use the environment variable or use a given port
+const PORT = process.env.PORT || 8080;
+
+// Create a server, uses `handleRequest` which is function that takes
+// care of providing requested data
+// const server = http.createServer(handleRequest);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello there')
+})
+
+// Start the server
+ap.listen(PORT, () => {
+  console.log('Server listening on: http://localhost:%s', PORT);
+});
